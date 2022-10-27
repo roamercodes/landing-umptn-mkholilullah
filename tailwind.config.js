15b0e16843651d9776689d37590e073bf9926786
roamercodes <mkholilullah29@gmail.com>
2022-10-27 23:10:43 +0700

    update index

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
+    // container: {
+    //   center: true,
+    //   padding: '16px',
+    // },
     extend: {},
+    // screens: {
+    //   '2xl': '1320px',
+    // }
   },
   plugins: [],
 }
